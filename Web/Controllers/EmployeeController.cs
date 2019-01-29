2f96272497ce7994384304f9b9ba8dceb21fe8f5
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2019-01-29 21:44:08 +0300

    First

---
+++ Web/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var result = new List<EmployeeViewModel>();
+            using (SignalR_ExampleEntities dbContext = new SignalR_ExampleEntities())
+            {
+                var db = dbContext.Employee.ToList();
+                foreach (var item in db)
+                {
+                    result.Add(new EmployeeViewModel {
+                        Id = item.Id,
+                        Name = item.Name,
+                        Surname = item.Surname
+                    });
+                }
+            }
+            return View(result);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }        
+    }
+}
---
+++ Web/Hubs/EmployeeHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Web.Hubs
+{
+    public class EmployeeHub : Hub
+    {
+        public void Create(string name, string surname)
+        {
+            using (SignalR_ExampleEntities dbContext = new SignalR_ExampleEntities())
+            {
+                dbContext.Employee.Add(new Employee { Name = name, Surname = surname});
+
+                dbContext.SaveChanges();
+
+                Clients.Caller.GetProcessStatus(true);
+
+                Clients.Others.GetValue(name,surname);
+            }
+        }
+    }
+}
